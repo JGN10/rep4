3a1ba7c3cb33ababc4085ad19f87184730b37ca8
JGN10 <jero.gutierrez.n@gmail.com>
2022-07-20 12:42:12 +0200

    segundo cambio de clase1

--- Basico/clase1.py
+++ Basico/clase1.py
@@ -7,5 +7,10 @@ class clase1:
         print(f'el valor de var_global es {var_global}')
         print(f'el valor de x es {x} ')
         print(f'el valor de y es {y} ')
+        x = 'cambiado dentro de funcion1 xxx'
+        y = 'cambiado dentro de funcion1 yyy'
+        print(f'el valor de x despues de cambiarlo es {x} ')
+        print(f'el valor de y despues de cambiarlo es {y} ')
+        return('aa','bb')
 
 
