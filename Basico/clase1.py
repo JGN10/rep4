986756a97a8d8a68c63cb7593e395b4d948a6610
JGN10 <jero.gutierrez.n@gmail.com>
2022-07-20 12:30:39 +0200

    cambios clase1

---
+++ Basico/clase1.py
@@ -0,0 +1,11 @@
+class clase1:
+    
+    def funcion1(x,y):
+        #global var_global
+        var_global = 'no es global'
+        print('dentro de clase1.funcion1')
+        print(f'el valor de var_global es {var_global}')
+        print(f'el valor de x es {x} ')
+        print(f'el valor de y es {y} ')
+
+
