2bb914567757302e87dd38509a51364465c56147
JGN10 <jero.gutierrez.n@gmail.com>
2022-07-22 15:02:33 +0200

    ejercicio_20

---
+++ funciones/funciones_20.py
@@ -0,0 +1,102 @@
+#from telnetlib import theNULL
+#import pandas as pd
+
+def generar_n_caracteres(n, caracter):
+    salida = ""
+    salida = caracter * n
+    #for i in range(n):
+    #    salida += caracter
+    return (salida)
+    #print(f'salida  {salida}')
+
+
+
+
+def procedimiento(lista):
+    for i in lista:
+        imprimir = ''
+        imprimir = 'X' * i
+        print(f'{imprimir}' )
+        #for j in range(len(i)):
+        #    imprimir +=
+
+
+
+def mas_larga(lista):
+    longitud = 0
+    palabra = ''
+    for i in lista:
+        if longitud <= len(i):
+            longitud = len(i)
+            palabra = i
+    return palabra, longitud
+
+#x,y = mas_larga(['1','12','123'])
+#print(x)
+#print(y)
+
+def filtrar_palabras(lista, n):
+    lista_aux = []
+    for i in lista:
+        if len(i) >= n:
+            lista_aux.append(i)
+    print(lista_aux) 
+
+filtrar_palabras(['123','22','222','4444'], 4)
+
+def c_mayusculas(cadena):
+    mayusculas = 0
+    cad_aux = ''
+    for i in cadena:
+        if i== i.upper():
+            mayusculas += 1
+            cad_aux += i
+    return mayusculas, cad_aux
+
+#m,c = c_mayusculas('bcdDDef')
+#print(m)
+#print(c)
+
+def main():
+    nombres = []
+    nombres_buscados = []
+    numero = int(input('introduce el número de nombres a introducir '))
+    letra = input('introduce la letra por la cual comparar ')
+    cuantos = 0
+    for i in range(numero):
+        nombres.append(input(f'introduce el nombre {i+1}'))
+    for j in nombres:
+        if j.startswith(letra):
+            nombres_buscados.append(j)
+            cuantos += 1
+    return nombres_buscados,cuantos
+
+#n,c = main()
+#print(n)
+#print(c)
+
+def contar_vocales():
+    cadena = input('introduce la cadena a comparar ')
+    #df = pd.DataFrame (columns= ['vocal','numero'])
+    #df_aux = pd.DataFrame
+    vocales = ['A','E','I','O','U']
+    numero = 0
+    for i in vocales:
+        numero = 0
+        for j in cadena:
+            if i==j.upper():
+                numero += 1
+        print(f'El número de {i} en la cadena {cadena} es {numero}')
+
+#contar_vocales()
+
+
+
+
+
+
+
+    
+
+
+
