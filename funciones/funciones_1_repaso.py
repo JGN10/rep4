da4309cab50298abfb0ff39968222391d7ed079c
JGN10 <jero.gutierrez.n@gmail.com>
2022-09-01 11:31:56 +0200

    primera subida repaso 1

---
+++ funciones/funciones_1_repaso.py
@@ -0,0 +1,9 @@
+def ejercicio_1(var1):
+    x = input (f'Introduce el valor a asignar a la variable {var1} ')
+    return (x)
+
+
+def ejercicio_2():
+    v1 = input ('Introduce el valor a asignar a la varible S ')
+    v2 = input ('Introduce el valor a asignar a la varible Y ')
+    return (v1,v2)
